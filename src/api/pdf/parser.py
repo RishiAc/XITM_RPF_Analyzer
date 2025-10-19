d27d5c8e5a2c8676af5a02294556a0858accbfa4
mcyoon24 <mcyoon24@gmail.com>
2025-10-19 14:07:02 -0400

    frontend initial commit with new chunking

--- src/api/pdf/parser.py
+++ src/api/pdf/parser.py
@@ -1,12 +1,29 @@
 from llama_parse import LlamaParse
 from llama_index.core.node_parser import SentenceSplitter
+import re
 
-def parse(file):
-    parser = LlamaParse(api_key="llx-y0lDjk4o2QVdB13CbRMdFdl3iEAZpBo6AbnsWFdGgBqgJb7l")
+def write_to_file(nodes, file):
+    with open(file, "w") as f:
+        for i, node in enumerate(nodes):
+            f.write(f"--- Chunk #{i} ---\n")
+            f.write(node.text + "\n")
+
+def parse(file, chunkSize: int = 64, chunkOverlap: int = 12):
+    parser = LlamaParse(
+        api_key="llx-y0lDjk4o2QVdB13CbRMdFdl3iEAZpBo6AbnsWFdGgBqgJb7l",  # See how to get your API key at https://docs.cloud.llamaindex.ai/api_key
+        parse_mode="parse_page_with_agent",  # The parsing mode
+        model="anthropic-sonnet-4.0",  # The model to use
+        high_res_ocr=True,  # Whether to use high resolution OCR (slower but more precise)
+        adaptive_long_table=True,  # Adaptive long table. LlamaParse will try to detect long table and adapt the output
+        outlined_table_extraction=True,  # Whether to try to extract outlined tables
+        output_tables_as_HTML=True,  # Whether to output tables as HTML in the markdown output
+    )
     parsed_doc = parser.load_data(file)
 
-    splitter = SentenceSplitter(chunk_size=512, chunk_overlap=75)
+    splitter = SentenceSplitter(chunk_size=chunkSize, chunk_overlap=chunkOverlap)
     nodes = splitter.get_nodes_from_documents(parsed_doc)
+
+    write_to_file(nodes, "rfp_chunks_with_chat.txt2")
 
     return nodes
 
@@ -18,9 +35,9 @@ def chunks_to_json(doc_id, nodes):
     for i, node in enumerate(nodes, start=1):  # start numbering at 1
         text = node.text.strip()
         text = text.replace("\r\n", " ")  # normalize Windows line endings
-        text = text.replace("\n\n", " ")
         text = text.replace("\n", " ")
-        text = text.replace("          ", " ")
+
+        text = re.sub(r"\s+", " ", text) # Simple Regex logic by Sriman S.
 
         output["chunks"].append({
             "chunk_num": i,
@@ -29,5 +46,3 @@ def chunks_to_json(doc_id, nodes):
 
     return output
 
-if __name__ == "__main__":
-    print(chunks_to_json("test", parse("/Users/nidhingangisetty/XITM_RPF_Analyzer/docs/rpfs/rfp-26-001-htdc.pdf")))
