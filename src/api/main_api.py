4a04a13ea16e5937ca3e7c891c05053c817f7429
Nidhin Gangisetty <nidhingangisetty@gmail.com>
2025-10-28 10:16:16 -0400

    llm_eval_api base

--- src/api/main_api.py
+++ src/api/main_api.py
@@ -1,4 +1,4 @@
-from . import vector_api, chunk_api
+from . import vector_api, chunk_api, llm_eval_api
 from fastapi import FastAPI
 
 
@@ -6,4 +6,5 @@ app = FastAPI(title="XITM RFP API", version="0.0.2")
 
 app.include_router(vector_api.router)
 app.include_router(chunk_api.router)
+app.include_router(llm_eval_api.router)
 
