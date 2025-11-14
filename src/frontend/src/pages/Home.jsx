3e34eacdb5162761ce2f344a3f06a4a57a0cdf4b
JonathanEarnest <earnest.jonathane@gmail.com>
2025-11-13 21:10:37 -0500

    Fixed some typos in code

--- src/frontend/src/pages/Home.jsx
+++ src/frontend/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./Home.css";
 import Navbar from "../components/Navbar";
 import { uploadPDF } from "../SupaBase/uploadPDF"; // adjust path if needed
